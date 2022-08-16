e4133d17316ea960582985d0bdc64af8f0919157
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-08-16 17:40:21 -0400

    personalized

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ root =Tk()
 root.geometry("400x400") #size of the window by default
 root.resizable(0,0) #to make the window size fixed
 #title of our window
-root.title("Weather App - AskPython.com")
+root.title("Kenneth's Weather App")
  
  
 # ----------------------Functions to fetch and display weather info
