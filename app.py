f4c53869da1c4b3a123f63384511a17581c15d36
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-08-16 17:39:09 -0400

    finished

--- app.py
+++ app.py
@@ -2,49 +2,68 @@ from tkinter import *
 import requests
 import json
 from datetime import datetime
-
-#initialize window
-
-root = Tk()
-root.geometry("400x400") #size of window
-root.resizable(0,0) #to make window size fixed
-root.title("Weather App by Kenneth")
-
+ 
+#Initialize Window
+ 
+root =Tk()
+root.geometry("400x400") #size of the window by default
+root.resizable(0,0) #to make the window size fixed
+#title of our window
+root.title("Weather App - AskPython.com")
+ 
+ 
+# ----------------------Functions to fetch and display weather info
 city_value = StringVar()
-
-#UTC -> local time
+ 
+ 
 def time_format_for_location(utc_with_tz):
     local_time = datetime.utcfromtimestamp(utc_with_tz)
     return local_time.time()
-
-#create the fetch method for the weather data
-
-def fetchWeatherData():
-    api_key = "0a138e73c9d1fff859ca0ae7fc0b6b46"
-    city_name = city_value.get() # we're going to use this method later to get the name of the city from user input
-    weather_url = "http://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&appid=" + api_key
-    response = requests.get(weather_url) #fetch url
-    weather_info = response.json()
-    tfield.delete("1.0", "end") #basically a e.preventDefault()
-
-    if weather_info['cod'] == 200:
-        kelvin = 273
-
-    
-    temp = int(weather_info['main']['temp'] - kelvin) # converting to kelvin
-    feels_like_temp = int(weather_info['main']['feels_like'] - kelvin)
-    pressure = weather_info['main']['pressure']
-    humidity = weather_info['main']['humidity']
-    wind_speed = weather_info['wind']['speed'] * 3.6
-    sunrise = weather_info['sys']['sunrise']
-    sunset = weather_info['sys']['sunset']
-    timezone = weather_info['timezone']
-    cloudy = weather_info['clouds']['all']
-    description = weather_info['weather'][0]['description']
  
-    sunrise_time = time_format_for_location(sunrise + timezone)
-    sunset_time = time_format_for_location(sunset + timezone)
-
+ 
+city_value = StringVar()
+ 
+def showWeather():
+    #Enter you api key, copies from the OpenWeatherMap dashboard
+    api_key = "0a138e73c9d1fff859ca0ae7fc0b6b46"  #sample API
+ 
+    # Get city name from user from the input field (later in the code)
+    city_name=city_value.get()
+ 
+    # API url
+    weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city_name + '&appid='+api_key
+ 
+    # Get the response from fetched url
+    response = requests.get(weather_url)
+ 
+    # changing response from json to python readable 
+    weather_info = response.json()
+ 
+ 
+    tfield.delete("1.0", "end")   #to clear the text field for every new output
+ 
+#as per API documentation, if the cod is 200, it means that weather data was successfully fetched
+ 
+ 
+    if weather_info['cod'] == 200:
+        kelvin = 273 # value of kelvin
+ 
+#-----------Storing the fetched values of weather of a city
+ 
+        temp = int(weather_info['main']['temp'] - kelvin)                                     #converting default kelvin value to Celcius
+        feels_like_temp = int(weather_info['main']['feels_like'] - kelvin)
+        pressure = weather_info['main']['pressure']
+        humidity = weather_info['main']['humidity']
+        wind_speed = weather_info['wind']['speed'] * 3.6
+        sunrise = weather_info['sys']['sunrise']
+        sunset = weather_info['sys']['sunset']
+        timezone = weather_info['timezone']
+        cloudy = weather_info['clouds']['all']
+        description = weather_info['weather'][0]['description']
+ 
+        sunrise_time = time_format_for_location(sunrise + timezone)
+        sunset_time = time_format_for_location(sunset + timezone)
+ 
 #assigning Values to our weather varaible, to display as output
          
         weather = f"\nWeather of: {city_name}\nTemperature (Celsius): {temp}°\nFeels like in (Celsius): {feels_like_temp}°\nPressure: {pressure} hPa\nHumidity: {humidity}%\nSunrise at {sunrise_time} and Sunset at {sunset_time}\nCloud: {cloudy}%\nInfo: {description}"
